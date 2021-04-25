9cbea4648ac2bcddaa68a681947ead2d618db581
альфир <alfir1990@.mail.ru>
2021-04-25 11:13:54 +0300

    Большие перемены (часть 1)

--- src/main.js
+++ src/main.js
@@ -1,21 +1,11 @@
 import MenuView from './view/menu.js';
-import InfoView from './view/trip-info.js';
-import TripCostView from './view/trip-cost.js';
 import FilterView from './view/filter.js';
-import SortingView from './view/sorting.js';
-import EventView from './view/event.js';
-import EventEditView from './view/event-edit.js';
 import EventListView from './view/event-list.js';
-import NoEventsView from './view/no_event_view.js';
 import {generateEvent} from './mock/event.js';
-import {render, replace, RenderPosition} from './utils/render.js';
+import {render, RenderPosition} from './utils/render.js';
+import TripPresenter from './presenter/trip.js';
 
 const EVENTS_COUNT = 20;
-
-const ButtonKeys = {
-  ESCAPE_SHORT: 'Esc',
-  ESCAPE_FULL: 'Escape',
-};
 
 const events = new Array(EVENTS_COUNT).fill().map(generateEvent);
 
@@ -28,57 +18,10 @@ const filterElement = document.querySelector('.trip-controls__filters');
 render(filterElement, new FilterView(), RenderPosition.BEFOREEND);
 
 const tripEventsElement = document.querySelector('.trip-events');
-render(tripEventsElement, new SortingView(), RenderPosition.BEFOREEND);
 
-const eventListComponent = new EventListView();
-render(tripEventsElement, eventListComponent, RenderPosition.BEFOREEND);
+const tripEventListComponent = new EventListView();
+render(tripEventsElement, tripEventListComponent, RenderPosition.BEFOREEND);
 
-const renderEvent = (event) => {
-  const parentElement = eventListComponent.getElement();
-  const eventComponent = new EventView(event);
-  const editEventComponent = new EventEditView(event);
+const tripPresenter = new TripPresenter(tripEventsElement, tripEventListComponent, tripMainElement);
 
-  const replaceCardToForm = () => {
-    replace(editEventComponent, eventComponent);
-  };
-
-  const replaceFormToCard = () => {
-    replace(eventComponent, editEventComponent);
-  };
-
-  const onEscKeyDown = (evt) => {
-    if (evt.key === ButtonKeys.ESCAPE_FULL || evt.key === ButtonKeys.ESCAPE_FULL) {
-      evt.preventDefault();
-      replaceFormToCard();
-      document.removeEventListener('keydown', onEscKeyDown);
-    }
-  };
-
-  eventComponent.setEditClickHandler(() => {
-    replaceCardToForm();
-    document.addEventListener('keydown', onEscKeyDown);
-  });
-
-  editEventComponent.setFormSubmitHandler(() => {
-    replaceFormToCard();
-    document.addEventListener('keydown', onEscKeyDown);
-  });
-
-  editEventComponent.setClickHandler(() => {
-    replaceFormToCard();
-    document.addEventListener('keydown', onEscKeyDown);
-  });
-
-  render(parentElement, eventComponent, RenderPosition.BEFOREEND);
-};
-
-if (events.length > 0) {
-  render(tripMainElement, new InfoView(events), RenderPosition.AFTERBEGIN);
-  const tripInfoElement = tripMainElement.querySelector('.trip-info');
-  render(tripInfoElement, new TripCostView(events), RenderPosition.BEFOREEND);
-  for (let i = 0; i < events.length; i++) {
-    renderEvent(events[i]);
-  }
-} else {
-  render(tripEventsElement, new NoEventsView(), RenderPosition.BEFOREEND);
-}
+tripPresenter.init(events);
--- src/view/trip-info.js
+++ src/view/trip-info.js
@@ -1,4 +1,5 @@
 import AbstractView from './abstract.js';
+import dayjs from 'dayjs';
 
 const createTripInfoTemplate = (events) => {
   const max_events_count_in_info_board = 3;
@@ -15,10 +16,13 @@ const createTripInfoTemplate = (events) => {
     cities = `${events[0].destination.city} &mdash; ${events[events.length - 1].destination.city}`;
   }
 
+  const dayStart = dayjs(events[0].timeStart).format('MMM D');
+  const dayEnd = dayjs(events[events.length - 1].timeEnd).format('MMM D');
+
   return `<section class="trip-main__trip-info  trip-info">
               <div class="trip-info__main">
                 <h1 class="trip-info__title">${cities}</h1>
-                <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+                <p class="trip-info__dates">${dayStart}&nbsp;&mdash;&nbsp;${dayEnd}</p>
               </div>
             </section>`;
 };
